6e51d42e27eb762cc04d3d0eca41395286c2cdc2
K0more <arch.phenon@gmail.com>
2022-04-24 13:59:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let countP = document.getElementById("count-p")
+let count = 0
+console.log(countP)
+function Add(){
+count = count + 1 
+countP.innerText = count
+}
+function Save(){
+    console.log(count)
+}
+function Reset(){
+    count = count - count
+    countP.innerText = count
+    
+}
